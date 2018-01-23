e729d2d9771915a47cd23bf2a30414f63c44fff3
Richard McGirr <rmcgirr83@gmail.com>
2018-01-23 06:17:23 -0500

    Change db column to null

---
+++ migrations/v3.php
@@ -0,0 +1,29 @@
+<?php
+/**
+*
+* @package Topic description
+* @copyright (c) 2018 Rich McGirr (RMcGirr83)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace rmcgirr83\topicdescription\migrations;
+
+class v3 extends \phpbb\db\migration\migration
+{
+	static public function depends_on()
+	{
+		return array('\rmcgirr83\topicdescription\migrations\v2');
+	}
+
+	public function update_schema()
+	{
+		return array(
+			'change_columns'    => array(
+				$this->table_prefix . 'topics'        => array(
+					'topic_desc'	=> array('TEXT_UNI', NULL),
+				),
+			),
+		);
+	}
+}
