79f4e62f41745d26147d7a01b389d3ea88ad746d
Richard McGirr <rmcgirr83@gmail.com>
2020-11-08 07:53:43 -0600

    Next version

--- event/listener.php
+++ event/listener.php
@@ -13,6 +13,10 @@ namespace rmcgirr83\topicdescription\event;
 /**
 * @ignore
 */
+use phpbb\auth\auth;
+use phpbb\request\request;
+use phpbb\template\template;
+use phpbb\language\language;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -20,8 +24,6 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 */
 class listener implements EventSubscriberInterface
 {
-	private $topic_desc = '';
-
 	/** @var \phpbb\auth\auth */
 	protected $auth;
 
@@ -31,8 +33,8 @@ class listener implements EventSubscriberInterface
 	/** @var \phpbb\template\template */
 	protected $template;
 
-	/** @var \phpbb\user */
-	protected $user;
+	/** @var \phpbb\language\language */
+	protected $language;
 
 	/**
 	* Constructor
@@ -40,19 +42,19 @@ class listener implements EventSubscriberInterface
 	* @param \phpbb\auth\auth					$auth				Auth object
 	* @param \phpbb\request\request				$request			Request object
 	* @param \phpbb\template\template           $template       	Template object
-	* @param \phpbb\user                        $user           	User object
+	* @param \phpbb\language\language           $language          	Language object
 	* @access public
 	*/
 	public function __construct(
-			\phpbb\auth\auth $auth,
-			\phpbb\request\request $request,
-			\phpbb\template\template $template,
-			\phpbb\user $user)
+		auth $auth,
+		request $request,
+		template $template,
+		language $language)
 	{
 		$this->auth = $auth;
 		$this->request = $request;
 		$this->template = $template;
-		$this->user = $user;
+		$this->language = $language;
 	}
 
 	/**
@@ -65,10 +67,11 @@ class listener implements EventSubscriberInterface
 	static public function getSubscribedEvents()
 	{
 		return array(
+			'core.acp_extensions_run_action_after'	=>	'acp_extensions_run_action_after',
 			'core.permissions'						=> 'add_permission',
 			'core.posting_modify_template_vars'		=> 'topic_data_topic_desc',
 			'core.posting_modify_submission_errors'		=> 'topic_desc_add_to_post_data',
-			'core.posting_modify_submit_post_before'		=> 'topic_desc_add',
+			'core.posting_modify_submit_post_before'	=> 'topic_desc_add',
 			'core.posting_modify_message_text'		=> 'modify_message_text',
 			'core.submit_post_modify_sql_data'		=> 'submit_post_modify_sql_data',
 			'core.viewtopic_modify_page_title'		=> 'topic_desc_add_viewtopic',
@@ -76,6 +79,20 @@ class listener implements EventSubscriberInterface
 			'core.search_modify_tpl_ary'			=> 'search_modify_tpl_ary',
 			'core.mcp_view_forum_modify_topicrow'	=> 'modify_topicrow',
 		);
+	}
+
+	/* Display additional metdate in extension details
+	*
+	* @param $event			event object
+	* @param return null
+	* @access public
+	*/
+	public function acp_extensions_run_action_after($event)
+	{
+		if ($event['ext_name'] == 'rmcgirr83/topicdescription' && $event['action'] == 'details')
+		{
+			$this->template->assign_var('S_BUY_ME_A_BEER_TOPICDESCRIPTION',true);
+		}
 	}
 
 	/**
@@ -97,11 +114,12 @@ class listener implements EventSubscriberInterface
 		$mode = $event['mode'];
 		$post_data = $event['post_data'];
 		$page_data = $event['page_data'];
-		$post_data['topic_desc'] = (!empty($post_data['topic_desc'])) ? $post_data['topic_desc'] : '';
+		$topic_desc = (!empty($post_data['topic_desc'])) ? $post_data['topic_desc'] : '';
+
 		if ($this->auth->acl_get('f_topic_desc', $event['forum_id']) && ($mode == 'post' || ($mode == 'edit' && $post_data['topic_first_post_id'] == $post_data['post_id'])))
 		{
-			$this->user->add_lang_ext('rmcgirr83/topicdescription', 'common');
-			$page_data['TOPIC_DESC'] = $this->request->variable('topic_desc', $post_data['topic_desc'], true);
+			$this->language->add_lang('common', 'rmcgirr83/topicdescription');
+			$page_data['TOPIC_DESC'] = $this->request->variable('topic_desc', $topic_desc, true);
 			$page_data['S_DESC_TOPIC'] = true;
 		}
 
@@ -110,12 +128,9 @@ class listener implements EventSubscriberInterface
 
 	public function topic_desc_add_to_post_data($event)
 	{
-		if ($this->auth->acl_get('f_topic_desc', $event['forum_id']))
-		{
-			$event['post_data'] = array_merge($event['post_data'], array(
-				'topic_desc'	=> $this->request->variable('topic_desc', '', true),
-			));
-		}
+		$event['post_data'] = array_merge($event['post_data'], array(
+			'topic_desc'	=> $this->request->variable('topic_desc', '', true),
+		));
 	}
 
 	public function topic_desc_add($event)
@@ -128,7 +143,7 @@ class listener implements EventSubscriberInterface
 	public function modify_message_text($event)
 	{
 		$event['post_data'] = array_merge($event['post_data'], array(
-			'topic_desc'	=> $this->request->variable('topic_desc', $event['post_data']['topic_desc'], true),
+			'topic_desc'	=> $this->request->variable('topic_desc', '', true),
 		));
 	}
 
@@ -137,10 +152,12 @@ class listener implements EventSubscriberInterface
 		$mode = $event['post_mode'];
 		$topic_desc = $event['data']['topic_desc'];
 		$data_sql = $event['sql_data'];
+
 		if (in_array($mode, array('post', 'edit_topic', 'edit_first_post')))
 		{
 			$data_sql[TOPICS_TABLE]['sql']['topic_desc'] = $topic_desc;
 		}
+
 		$event['sql_data'] = $data_sql;
 	}
 
@@ -153,6 +170,7 @@ class listener implements EventSubscriberInterface
 	public function modify_topicrow($event)
 	{
 		$row = $event['row'];
+
 		if (!empty($row['topic_desc']))
 		{
 			$topic_row = $event['topic_row'];
@@ -164,6 +182,7 @@ class listener implements EventSubscriberInterface
 	public function search_modify_tpl_ary($event)
 	{
 		$row = $event['row'];
+
 		if ($event['show_results'] == 'topics' && !empty($row['topic_desc']))
 		{
 			$tpl_array = $event['tpl_ary'];
